3ccd50f701b1d5d91e92bdbd036b0e03cd0772d4
Rishit Shah <aryanrishit1@gmail.com>
2025-08-08 19:56:39 +0530

    Commit 9: Partially converted project into React, just the index file and under construction index file.

---
+++ Portfolio-Site/src/UnderConstruction.jsx
@@ -0,0 +1,12 @@
+
+function UnderConstruction() {
+
+    return(
+        <div id="big-picture">
+            <div class="folder" id="main"></div>
+            <div class="folder" id="top"></div>
+        </div>
+    );
+}
+
+export default UnderConstruction;
