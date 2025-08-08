a32db8b7ec09075c01bc08086dfd9ed66ec0e566
Rishit Shah <aryanrishit1@gmail.com>
2025-08-08 20:02:06 +0530

    Commit 10: Updated the main.jsx file to handle the new UnderConstruction and Main components.

--- Portfolio-Site/src/main.jsx
+++ Portfolio-Site/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import UnderConstruction from './UnderConstruction.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render (
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/under-construction" element={<UnderConstruction />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
